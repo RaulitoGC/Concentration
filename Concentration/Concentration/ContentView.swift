137d2bc1f7f344577cbb31c31a3c239bbad3d291
Raul Guzman <rguzmanc.161@gmail.com>
2020-04-10 16:50:27 -0500

    Feat: Initial structure

---
+++ Concentration/Concentration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by Raul Jonathan Guzman Condor on 4/10/20.
+//  Copyright © 2020 Raul's House. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

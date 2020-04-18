1c92594e416cee9fec40e03d8986ff84f8e89250
Raul Guzman <rguzmanc.161@gmail.com>
2020-04-18 15:22:45 -0500

    Feat: Add storyboard

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Raul Jonathan Guzman Condor on 4/10/20.
+//  Copyright © 2020 Raul's House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
